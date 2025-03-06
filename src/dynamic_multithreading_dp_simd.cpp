adcbcacf53428c41038cb9d66e7d39d27bf12b8b
Alexander Sohn <github@asohn.de>
2025-03-06 12:15:46 +0100

    Use openmp simd pragma.

---
+++ src/dynamic_multithreading_dp_simd.cpp
@@ -0,0 +1,51 @@
+#include "mandelbrot.hpp"
+
+int64_t mandelbrot_computation(ofstream &matrix_out, bool output)
+{
+    uint16_t *const image = new uint16_t[HEIGHT * WIDTH];
+
+    const auto start = chrono::steady_clock::now();
+    #pragma omp parallel for simd collapse(2) schedule(dynamic, 100)
+    for (uint row = 0; row < HEIGHT; ++row) {
+        for (uint col = 0; col < WIDTH; ++col) {
+            const complex<double> c((col * STEP + MIN_X), (row * STEP + MIN_Y));
+
+            // z = z^2 + c
+            complex<double> z(0, 0);
+            for (uint16_t i = 1; i <= ITERATIONS; ++i)
+            {
+                z = pow(z, 2) + c;
+
+                // If it is convergent
+                image[row*WIDTH+col] = i;
+                if (abs(z) >= 2)
+                {
+                    break;
+                }
+            }
+        }
+    }
+    const auto end = chrono::steady_clock::now();
+    auto difference = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    cerr << "Time elapsed: "
+         << difference
+         << " milliseconds." << endl;
+    if (output) { 
+        for (int row = 0; row < HEIGHT; row++)
+        {
+            for (int col = 0; col < WIDTH; col++)
+            {
+                matrix_out << image[row * WIDTH + col];
+
+                if (col < WIDTH - 1)
+                    matrix_out << ',';
+            }
+            if (row < HEIGHT - 1)
+                matrix_out << endl;
+        }
+        matrix_out.close();
+    }
+
+    delete[] image; // It's here for coding style, but useless
+    return difference;
+}
---
+++ src/no_complex_sp_simd.cpp
@@ -0,0 +1,59 @@
+
+#include "mandelbrot.hpp"
+#define SIMD_LEN 8
+#define ALIGNED_LENGTH ((HEIGHT * WIDTH) / SIMD_LEN + 1) * SIMD_LEN
+
+int64_t mandelbrot_computation(ofstream &matrix_out, bool output)
+{
+
+    //uint16_t *const image = new uint16_t[ALIGNED_LENGTH];
+    uint16_t* image = (uint16_t*) std::aligned_alloc(sizeof(float) * SIMD_LEN, ALIGNED_LENGTH * sizeof(uint16_t));
+
+    const auto start = chrono::steady_clock::now();
+
+    #pragma omp parallel for simd aligned(image:sizeof(float)*SIMD_LEN) simdlen(SIMD_LEN) safelen(SIMD_LEN) schedule(simd:dynamic, 128)
+    for (uint pos = 0; pos < ALIGNED_LENGTH; ++pos) {
+        const float c_re = (float)(pos % WIDTH) * (float) STEP + (float) MIN_X;
+        const float c_im = (float)(pos / WIDTH)* (float) STEP + (float) MIN_Y;
+
+        float z_re = c_re;
+        float z_im = c_im;
+        uint16_t iter = 1;
+
+        for (; iter < ITERATIONS; ++iter) {
+            float zr2 = z_re * z_re;
+            float zi2 = z_im * z_im;
+            if (zr2 + zi2 >= 4.0f)
+                break;
+            float new_z_re = zr2 - zi2 + c_re;
+            float new_z_im = 2.0f * z_re * z_im + c_im;
+            z_re = new_z_re;
+            z_im = new_z_im;
+        }
+        image[pos] = iter;
+    }
+
+    const auto end = chrono::steady_clock::now();
+    auto difference = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    cerr << "Time elapsed: "
+         << difference
+         << " milliseconds." << endl;
+    if (output) { 
+        for (int row = 0; row < HEIGHT; row++)
+        {
+            for (int col = 0; col < WIDTH; col++)
+            {
+                matrix_out << image[row * WIDTH + col];
+
+                if (col < WIDTH - 1)
+                    matrix_out << ',';
+            }
+            if (row < HEIGHT - 1)
+                matrix_out << endl;
+        }
+        matrix_out.close();
+    }
+
+    delete[] image; // It's here for coding style, but useless
+    return difference;
+}
