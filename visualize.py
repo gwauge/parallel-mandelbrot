36ced041690da16677a8ce682e80e6c45ec57739
Alexander Sohn <github@asohn.de>
2025-03-12 18:19:57 +0100

    Improve visualization script.

--- visualize.py
+++ visualize.py
@@ -5,12 +5,18 @@ import numpy as np
 
 
 def main(input_file: str, output_file: str | None):
-    image = np.loadtxt(input_file, delimiter=",", dtype=np.uint16)
+    image = np.log(np.loadtxt(input_file, delimiter=",", dtype=np.uint16))
 
-    plt.imshow(image, interpolation="none")
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.axis("off")
+    plt.margins(0, 0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.imshow(image, interpolation="none", cmap="BuGn", aspect="auto")
 
     if output_file:
-        plt.savefig(output_file)
+        plt.savefig(output_file, dpi=2400, bbox_inches=0, pad_inches = 0)
     else:
         plt.show()
 
